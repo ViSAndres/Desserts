68922bac2204ffe24ce0c53d98a84a92f38f872d
ViSAndres <carlosneira1997@gmail.com>
2024-08-17 20:11:55 -0400

    Initial Commit

---
+++ FetchChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FetchChallenge
+//
+//  Created by Carlos Neira on 8/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchChallenge/FetchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchChallengeApp.swift
+//  FetchChallenge
+//
+//  Created by Carlos Neira on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
