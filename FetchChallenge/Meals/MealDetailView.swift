acca98139af87aee6b0b530e552ea828edbe22ed
ViSAndres <carlosneira1997@gmail.com>
2024-08-19 16:07:34 -0400

    Adding styling to MealDetailsView

--- FetchChallenge/Meals/MealDetailView.swift
+++ FetchChallenge/Meals/MealDetailView.swift
@@ -12,7 +12,83 @@ struct MealDetailView: View {
     
     
     var body: some View {
-        Text(meal.strMeal)
+        Group {
+            ScrollView {
+                VStack(spacing: 0) {
+                    HeaderView()
+                    
+                    BodyView()
+                }
+            }
+            .background(.gray)
+            .cornerRadius(16)
+            .overlay(
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(Color(red: 0.12941176470588237, green: 0.16862745098039217, blue: 0.16470588235294117), lineWidth: 1)
+            )
+            .padding(.horizontal, 20)
+        }
+        // TODO: Add colors to xcode
+        .background(Color(red: 0.12941176470588237, green: 0.16862745098039217, blue: 0.16470588235294117))
+//        .ignoresSafeArea()
+    }
+    
+    @ViewBuilder
+    func HeaderView() -> some View {
+            VStack {
+                ImageView()
+                
+                Text(meal.strMeal)
+                    .font(.title)
+            }
+    }
+    
+    
+    
+    @ViewBuilder
+    func ImageView() -> some View {
+        let urlStr = meal.strMealThumb ?? "No URL"
+        
+        AsyncImage(url: URL(string: urlStr)) { image in
+            image.resizable()
+        } placeholder: {
+            Image(systemName: "carrot")
+        }
+        .frame(maxWidth: .infinity, maxHeight: 300, alignment: .topLeading)
+    }
+    
+    @ViewBuilder
+    func BodyView() -> some View {
+        VStack(alignment: .leading) {
+            Text("Instructions")
+                .font(.title2)
+            
+            Text(meal.strInstructions ?? "Idk just cook it")
+            
+            IngredientsView()
+        }
+        .padding()
+    }
+    
+    @ViewBuilder
+    func IngredientsView() -> some View {
+        let ingredients = meal.getIngredients()
+        
+        VStack(alignment: .leading) {
+            Text("Ingredients")
+                .font(.title2)
+            
+            ForEach(ingredients.keys.sorted(), id: \.self) { key in
+                if let measurement = ingredients[key] {
+                    if key != "" {
+                        if measurement != " " {
+                            Text("\(measurement) of \(key)")
+                        } else { Text(key) }
+                    }
+                }
+            }
+        }
+        .padding(.top, 10)
     }
 }
 
--- FetchChallenge/Models/MealDetails.swift
+++ FetchChallenge/Models/MealDetails.swift
@@ -65,4 +65,31 @@ public struct Details: Codable {
     let strImageSource: String?
     let strCreativeCommonsConfirmed: String?
     let dateModified: String?
+    
+    func getIngredients() -> [String: String] {
+        var ingredients: [String: String] = [:]
+        
+        if let strIngredient1 = strIngredient1 { ingredients[strIngredient1] = strMeasure1 ?? "" }
+        if let strIngredient2 = strIngredient2 { ingredients[strIngredient2] = strMeasure2 ?? "" }
+        if let strIngredient3 = strIngredient3 { ingredients[strIngredient3] = strMeasure3 ?? "" }
+        if let strIngredient4 = strIngredient4 { ingredients[strIngredient4] = strMeasure4 ?? "" }
+        if let strIngredient5 = strIngredient5 { ingredients[strIngredient5] = strMeasure5 ?? "" }
+        if let strIngredient6 = strIngredient6 { ingredients[strIngredient6] = strMeasure6 ?? "" }
+        if let strIngredient7 = strIngredient7 { ingredients[strIngredient7] = strMeasure7 ?? "" }
+        if let strIngredient8 = strIngredient8 { ingredients[strIngredient8] = strMeasure8 ?? "" }
+        if let strIngredient9 = strIngredient9 { ingredients[strIngredient9] = strMeasure9 ?? "" }
+        if let strIngredient10 = strIngredient10 { ingredients[strIngredient10] = strMeasure10 ?? "" }
+        if let strIngredient11 = strIngredient11 { ingredients[strIngredient11] = strMeasure11 ?? "" }
+        if let strIngredient12 = strIngredient12 { ingredients[strIngredient12] = strMeasure12 ?? "" }
+        if let strIngredient13 = strIngredient13 { ingredients[strIngredient13] = strMeasure13 ?? "" }
+        if let strIngredient14 = strIngredient14 { ingredients[strIngredient14] = strMeasure14 ?? "" }
+        if let strIngredient15 = strIngredient15 { ingredients[strIngredient15] = strMeasure15 ?? "" }
+        if let strIngredient16 = strIngredient16 { ingredients[strIngredient16] = strMeasure16 ?? "" }
+        if let strIngredient17 = strIngredient17 { ingredients[strIngredient17] = strMeasure17 ?? "" }
+        if let strIngredient18 = strIngredient18 { ingredients[strIngredient18] = strMeasure18 ?? "" }
+        if let strIngredient19 = strIngredient19 { ingredients[strIngredient19] = strMeasure19 ?? "" }
+        if let strIngredient20 = strIngredient20 { ingredients[strIngredient20] = strMeasure20 ?? "" }
+        
+        return ingredients
+    }
 }
